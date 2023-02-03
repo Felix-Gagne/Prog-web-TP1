cf21dba1fd79667f8cd2f0cef6e5130f51ea5ccd
Félix Gagné <2070094@cegepmontpetit.ca>
2023-02-03 09:44:47 -0500

    Création du projet et installation des dépendances
    Création du projet angular pour le TP1 et ajout de bootstrap et jquery.

---
+++ TP1_FELIX_GAGNE/src/app/app.component.spec.ts
@@ -0,0 +1,31 @@
+import { TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+
+describe('AppComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  });
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'TP1_FELIX_GAGNE'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('TP1_FELIX_GAGNE');
+  });
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.content span')?.textContent).toContain('TP1_FELIX_GAGNE app is running!');
+  });
+});
